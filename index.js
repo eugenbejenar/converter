9fae777cb7e91100858cfa5b452a7227af4618ab
Eugen <you@example.com>
2019-12-09 23:45:42 +0200

    v2

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ function convert() {
     var selectT = document.getElementById('moneySelect').value;
     var inputValue = document.getElementById('moneyInput').value;
     if (selectT === "usd") {
-        var usdToMdl = inputValue * 17.43 + 'MDL';
-        alert(usdToMdl);
+        var usdToMdl = inputValue * 17.43 + ' MDL';
+        // alert(usdToMdl);
+        document.getElementById('result').innerHTML = usdToMdl;
+
     } else {
-        var mdlToUsd = inputValue / 17.43 + 'USD';
-        alert(mdlToUsd);
+        var mdlToUsd = inputValue / 17.43 + ' USD';
+        // alert(mdlToUsd);
+        document.getElementById('result').innerHTML = mdlToUsd;
     }
 }
